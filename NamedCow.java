cae1aba555293255753aa992ea0a165e46bcef9f
Mariann-lowellapcs <31248014+Mariann-lowellapcs@users.noreply.github.com>
2017-10-19 21:59:35 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,16 @@
-class NamedCow
-{
-
+class NamedCow extends Cow {  
+    private String myName; 
+    private String myType;     
+     private String mySound;      
+    public NamedCow(String type, String name, String sound){
+    myType = type;
+    myName = name;
+    mySound = sound;
+}
+public NamedCow(){
+ myName ="Elsie";
+}
+     public String getName(){
+        return myName;
+     }
 }
